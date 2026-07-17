46330523aa56476ec1196709f90d92a575b7e629
Smk777 <youssefalmouna8@gmail.com>
2026-07-17 13:04:55 +0100

    up

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 import sequelize from "./config/database.js";
 import ps from "ps";
 import express from "express";
+import dotenv from "dotenv"
 
 dotenv.config();
 const app = express()
 app.use(express.json())
 
 const main = async () => {
-    app.listen(3000, () => {
-        console.log("server is running")
+    try {
+           await sequelize.authenticate();
+    console.log("database is connect");
+    app.listen(3000, () =>  {
+        console.log("server is running on port 3000")
+
     })
+ 
+    } catch (err) {
+        console.log({error: err.message})
+        
+    }
 
 }
+main()
